86101fe20b3d98db1676d8de1b49b2ab17be8456
oleksiikrutko <krutko.av@gmail.com>
2021-09-16 10:51:50 +0300

    version 2.38 add checking a correct companyId in an updated Developer

--- src/main/java/ua/goit/controller/HandlerUpdateDeveloper.java
+++ src/main/java/ua/goit/controller/HandlerUpdateDeveloper.java
@@ -1,7 +1,11 @@
 package ua.goit.controller;
 
+import ua.goit.model.Company;
 import ua.goit.model.Developer;
+import ua.goit.service.CompanyService;
 import ua.goit.service.DeveloperService;
+
+import java.util.Optional;
 
 public class HandlerUpdateDeveloper extends HandlerMenu{
     public HandlerUpdateDeveloper(HandlerMenu handler) {
@@ -16,13 +20,18 @@ public class HandlerUpdateDeveloper extends HandlerMenu{
                 "{name}|{gender}|{age}|{salary}|{companyId}");
         String next = scanner.next();
         String[] split = next.split("\\|");
+        Long companyId = Long.valueOf(split[4]);
+        while (Optional.empty().equals(new CompanyService().findById(Company.class, companyId))) {
+            System.out.println("No one company with id = " + companyId + "\nenter another id company:");
+            companyId = scanner.nextLong();
+        }
         Developer.builder()
                 .id(Long.valueOf(command[2]))
                 .name(split[0])
                 .gender(split[1])
                 .age(Integer.valueOf(split[2]))
                 .salary(Integer.valueOf(split[3]))
-                .companyId(Long.valueOf(split[4]))
+                .companyId(companyId)
                 .build();
         Developer updateDeveloper = new DeveloperService().updateEntity(Developer.class, developer);
         System.out.println("updated developer: " + updateDeveloper.toString());
