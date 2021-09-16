c32c69b3fdc4b16be5876636d226f1194bf81bae
oleksiikrutko <krutko.av@gmail.com>
2021-09-16 17:36:24 +0300

    version 2.40

--- src/main/java/ua/goit/controller/HandlerCreateProject.java
+++ src/main/java/ua/goit/controller/HandlerCreateProject.java
@@ -1,7 +1,13 @@
 package ua.goit.controller;
 
+import ua.goit.model.Company;
+import ua.goit.model.Customer;
 import ua.goit.model.Project;
+import ua.goit.service.CompanyService;
+import ua.goit.service.CustomerService;
 import ua.goit.service.ProjectService;
+
+import java.util.Optional;
 
 public class HandlerCreateProject extends HandlerMenu{
     public HandlerCreateProject(HandlerMenu handler) {
@@ -26,12 +32,22 @@ public class HandlerCreateProject extends HandlerMenu{
                 "( e.g. Alpha|1_000_000|2021-09-09|3|1)");
         String next = scanner.next();
         String[] split = next.split("\\|");
+        Long companyId = Long.valueOf(split[3]);
+        while (Optional.empty().equals(new CompanyService().findById(Company.class, companyId))) {
+            System.out.println("No one company with id = " + companyId + "\nenter another id company:");
+            companyId = scanner.nextLong();
+        }
+        Long customerId = Long.valueOf(split[4]);
+        while (Optional.empty().equals(new CustomerService().findById(Customer.class, customerId))) {
+            System.out.println("No one customer with id = " + customerId + "\nenter another id customer:");
+            customerId = scanner.nextLong();
+        }
         return Project.builder()
                 .name(split[0])
                 .cost(Integer.valueOf(split[1]))
                 .startDate(split[2])
-                .companyId(Long.valueOf(split[3]))
-                .companyId(Long.valueOf(split[4]))
+                .companyId(companyId)
+                .customerId(customerId)
                 .build();
     }
 }
