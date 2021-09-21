b5c4c5e518fb33f428aaea5f8c32bb9ef5eb3196
oleksiikrutko <krutko.av@gmail.com>
2021-09-22 00:58:51 +0300

    version 2.44 fix bugs

--- src/main/java/ua/goit/controller/HandlerUpdateProject.java
+++ src/main/java/ua/goit/controller/HandlerUpdateProject.java
@@ -1,7 +1,13 @@
 package ua.goit.controller;
 
+import ua.goit.model.Company;
+import ua.goit.model.Customer;
 import ua.goit.model.Project;
+import ua.goit.service.CompanyService;
+import ua.goit.service.CustomerService;
 import ua.goit.service.ProjectService;
+
+import java.util.Optional;
 
 public class HandlerUpdateProject extends HandlerMenu{
     public HandlerUpdateProject(HandlerMenu handler) {
@@ -16,13 +22,23 @@ public class HandlerUpdateProject extends HandlerMenu{
                 "{name}|{cost}|{startDate}|{companyId}|{customerId}");
         String next = scanner.next();
         String[] split = next.split("\\|");
+        Long companyId = Long.valueOf(split[3]);
+        while (Optional.empty().equals(new CompanyService().findById(Company.class, companyId))) {
+            System.out.println("No one company with id = " + companyId + "\nenter another id company:");
+            companyId = scanner.nextLong();
+        }
+        Long customerId = Long.valueOf(split[4]);
+        while (Optional.empty().equals(new CustomerService().findById(Customer.class, customerId))) {
+            System.out.println("No one customer with id = " + customerId + "\nenter another id customer:");
+            customerId = scanner.nextLong();
+        }
         Project updatedProject = Project.builder()
                 .id(Long.valueOf(command[2]))
                 .name(split[0])
                 .cost(Integer.valueOf(split[1]))
                 .startDate(split[2])
-                .companyId(Long.valueOf(split[3]))
-                .customerId(Long.valueOf(split[4]))
+                .companyId(Long.valueOf(companyId))
+                .customerId(Long.valueOf(customerId))
                 .build();
         System.out.println("updated project: " + new ProjectService().updateEntity(Project.class, updatedProject).toString());
     }
